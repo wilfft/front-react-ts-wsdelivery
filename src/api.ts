2c29484cb24c6e5a2f7a46eb9e9f1317499e4f7f
wilfft <68745408+wilfft@users.noreply.github.com>
2021-03-02 01:49:56 -0300

    feat: adicionado _redirects

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrdemPayload } from "./Ordens/types";
 
-const API_URL = "https://spring-app-delivery.herokuapp.com";
+const API_URL = process.env.REACT_APP_API_URL;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export function carregaProdutos() {
